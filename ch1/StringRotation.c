ff93002ce071f4f6321be1f540643880f5e34d4b
Oliver Albertini <oliver.ruben@gmail.com>
2017-09-22 16:26:43 -0700

    Implement the easy solution StringRot

--- ch1/StringRotation.c
+++ ch1/StringRotation.c
@@ -8,6 +8,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdbool.h>
+#include<stdlib.h>
 
 // check if s1 is part of s2
 bool isSubstring(char *s1, char *s2)
@@ -36,8 +37,25 @@ bool isStringRotation(char *s1, char *s2)
                return isSubstring(p + 1, s2_hold);
    return false;
 }
+
+bool isStringRotationEasy(char *s1, char *s2)
+{
+   if (strlen(s1) != strlen(s2))
+      return false;
+   char *s1s1 = malloc(2 * strlen(s1) + 1);
+   int i;
+   for (i = 0; i < 2 * strlen(s1); ++i)
+      s1s1[i] = s1[i % strlen(s1)];
+   s1s1[i] = '\0';
+
+   return isSubstring(s2, s1s1);
+}
+
 int main(int argc, char *argv[])
 {
-   printf("%d\n", isStringRotation("waterbottle", "erbottlewat"));
+   if (isStringRotationEasy(argv[1], argv[2]))
+      printf("%s is a rotation of %s.\n", argv[1], argv[2]);
+   else
+      printf("%s is not a rotation of %s.\n", argv[1], argv[2]);
    return 0;
 }
